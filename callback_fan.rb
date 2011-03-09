b6594072f0da81d112d12d9e845a964efa2ae078
Abhishek Yadav <abhishek@tenmiles.com>
2011-03-09 15:49:33 +0530

    added facebook callback

--- callback_fan.rb
+++ callback_fan.rb
@@ -1,11 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 
-URL = "http://localhost:3000/callback/gihub"
+Url_gh = "http://localhost:3000/callback/gihub"
+Url_fb = "http://localhost:3000/callback/facebook"
 
 get '/callback/github' do
   code = params[:code]
-  href = "#{URL}?code=#{code}"
+  href = "#{Url_gh}?code=#{code}"
   response = "<a href=\"#{href}\">Go</a>"
 end
 
+get '/callback/facebook' do
+  code = params[:code]
+  href = "#{Url_fb}?code=#{code}"
+  response = "<a href=\"#{href}\">Go</a>"
+end
